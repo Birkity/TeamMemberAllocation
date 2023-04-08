8559fe89627216d8f1f514af9e4f5dc7b7ae7c3d
Birkity <lilyBluestorm7@gmail.com>
2023-04-07 23:47:18 -0700

    CSS minor edit, ternary operation for employee gender

--- src/Employees.js
+++ src/Employees.js
@@ -91,12 +91,31 @@ const Employees = () => {
   ]);
   return (
     <main className="container">
+      <div class="row justify-content-center mt-3 mb-3">
+        <div class="col-6">
+          <select className="form-select form-select-lg">
+            <option value="TeamA">TeamA</option>
+            <option value="TeamB">TeamB</option>
+            <option value="TeamC">TeamC</option>
+            <option value="TeamD">TeamD</option>
+          </select>
+        </div>
+      </div>
+      {/** bootstrap card rendering for employees */}
       <div className="row justify-content-center mt-3 mb-3">
         <div className="col-8">
-          <div class = "card-collection">
+          <div class="card-collection">
             {employees.map((employee) => (
-              <div id={employee.id} className="card">
-                <img src={femaleProfile} className="card-img-top" />
+              <div
+                id={employee.id}
+                className="card m-2"
+                style={{ cursor: "pointer" }}
+              >
+                {employee.gender === "male" ? (
+                  <img src={maleProfile} className="card-img-top" />
+                ) : (
+                  <img src={femaleProfile} className="card-img-top" />
+                )}
                 <div className="card-body">
                   <h5 className="card-title">Full Name: {employee.fullName}</h5>
                   <p className="card-text">
